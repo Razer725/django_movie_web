efb8bf22fc0572db4da4085ec550cfe4b85f4629
Ivan Krasilnikov <krasivan93@gmail.com>
2020-10-25 16:33:50 +0200

    Heroku

--- django_movie_web/settings.py
+++ django_movie_web/settings.py
@@ -222,7 +222,7 @@ RECAPTHCA_DEFAULT_ACTION = 'generic'
 RECAPTCHA_SCORE_THRESHOLD = 0.5
 
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 SITE_ID = 1
